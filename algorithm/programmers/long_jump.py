b7a5d8cd774f2a57ac4f72e56692e6be18f2eaaa
Ba-Da, Kim <qkek984@naver.com>
2020-09-24 09:51:37 +0900

    dp
    dp

--- algorithm/programmers/long_jump.py
+++ algorithm/programmers/long_jump.py
@@ -1,31 +1,5 @@
-
 def solution(n):
-    dic = {0:1}
-    answer = 0
-    for i in range(0,n):
-        tmp={}
-        min_v=987654321
-        for key in dic:
-            a = key + 1
-            b = key + 2
-            min_v = min(min_v,b)
-            if a >= n:
-                if a ==n:
-                    answer += dic[key]
-            elif a in tmp:
-                tmp[a] += dic[key]
-            else:
-                tmp[a] = dic[key]
-            
-            if b >= n:
-                if b == n:
-                    answer += dic[key]
-            elif b in tmp:
-                tmp[b] += dic[key]
-            else:
-                tmp[b] = dic[key]
-        if min_v > 2000:
-            break
-        else:
-            dic = tmp
-    return answer%1234567
+    dp=[0,1,2]
+    for i in range(3,n+1):
+        dp.append((dp[i-1]+dp[i-2])%1234567)
+    return dp[n]
