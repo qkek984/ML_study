33ea6f87bd057fe4dc0ab525f39199ed0dc6cb03
Ba-Da, Kim <qkek984@naver.com>
2020-10-21 11:30:01 +0900

    shorter
    shorter

--- algorithm/sort/mergeSort.py
+++ algorithm/sort/mergeSort.py
@@ -1,27 +1,23 @@
 def merge(left,right):
-    mArr = []
-    while len(left) > 0 or len(right) > 0:
-        if len(left)> 0 and len(right) > 0:
+    tmp=[]
+    while left or right:
+        if left and right:
             if left[0] <= right[0]:
-                mArr.append(left[0])
-                left = left[1:]
+                tmp.append(left.pop(0))
             else:
-                mArr.append(right[0])
-                right = right[1:]
-        elif len(left) > 0:
-            mArr.append(left[0])
-            left = left[1:]
-        elif len(right) > 0:
-            mArr.append(right[0])
-            right = right[1:]
-    return mArr
+                tmp.append(right.pop(0))
+        elif left:
+            tmp.append(left.pop(0))
+        else:
+            tmp.append(right.pop(0))
+    return tmp
 
-def mergeSort(arr):
+def mergeSort(arr): 
     if len(arr) <= 1:
         return arr
     mid = len(arr)//2
     left = arr[:mid]
     right = arr[mid:]
-    left = mergeSort(left)
-    right = mergeSort(right)
+    left=mergeSort(left)
+    right=mergeSort(right)
     return merge(left,right)
