b7a84f8c13c1298a6fd715a1e60356407383aa88
Ba-Da, Kim <qkek984@naver.com>
2021-01-27 10:10:47 +0900

    dp memorization
    dp memorization

--- algorithm/beakjoon/easystair.py
+++ algorithm/beakjoon/easystair.py
@@ -1,16 +1,14 @@
 n = int(input())
-dp=[0,1,1,1,1,1,1,1,1,1]
-for iter in range(1,n):
-    tmp=[0,0,0,0,0,0,0,0,0,0]
-    for i in range(0,10):
-        if dp[i] == 0:
-            continue
-        elif i==0:
-            tmp[1] += dp[i]
-        elif i == 9:
-            tmp[8] += dp[i]
+dp = [[0,0,0,0,0,0,0,0,0,0] for _ in range(101)]
+dp[1] = [0,1,1,1,1,1,1,1,1,1]
+
+for i in range(2,n+1):
+    for j in range(0,10):
+        if j==0:
+            dp[i][1] += dp[i-1][j]
+        elif j==9:
+            dp[i][8] += dp[i-1][j]
         else:
-            tmp[i-1] += dp[i]
-            tmp[i+1] += dp[i]
-    dp = tmp
-print(sum(dp)%1000000000)
+            dp[i][j-1] += dp[i-1][j]
+            dp[i][j+1] += dp[i-1][j]
+print(sum(dp[n])%1000000000)
